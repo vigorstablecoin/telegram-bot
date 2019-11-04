20ae1228abd8eb63ba7a14f7962ecd0d1ce7be86
MrToph <christoph.michel.1992@gmail.com>
2019-11-04 16:54:20 +0800

    Update help links to GitHub issues

--- src/bot/commands/help.ts
+++ src/bot/commands/help.ts
@@ -1,7 +1,7 @@
 import { Telegraf, ContextMessageUpdate } from 'telegraf'
 
 export const getHelpMessage = () => `Current bot commands are "/settings", "/help".
-For feature requests and bug reports please [open an issue in GitHub](https://github.com/vigorstablecoin/telegram-bot).`
+For feature requests and bug reports please [open an issue in GitHub](https://github.com/vigorstablecoin/telegram-bot/issues).`
 
 export default function setupHelp(bot: Telegraf<ContextMessageUpdate>) {
   bot.command(['help'], (ctx) => {
